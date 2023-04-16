b8c43f8f10057f77c35d39efe0ce7635931f6f05
ThatXliner <bryan.hu.2020@gmail.com>
2023-04-15 23:03:34 -0700

    Minor link fixes

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,12 +4,11 @@ import react from "@astrojs/react";
 import remarkToc from "remark-toc";
 import remarkCollapse from "remark-collapse";
 import sitemap from "@astrojs/sitemap";
-import { SITE } from "./src/config";
 
 // https://astro.build/config
 export default defineConfig({
-  base: '/blog',
-  site: SITE.website,
+  base: "/blog",
+  site: "https://thatxliner.github.io",
   integrations: [
     tailwind({
       config: {
